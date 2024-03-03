6f46c405cb86c8db137435faa36a60343d7f063a
Rory O Donoghue <irelander99@mail.com>
2024-03-03 22:06:19 +0000

    Commit 12 : Adding Collision Function to Script, in order to change Worms Colour on Collision

--- 2024_BSCH_CompSci_R_O_D_3053007/Assets/MaterialChanger.cs
+++ 2024_BSCH_CompSci_R_O_D_3053007/Assets/MaterialChanger.cs
@@ -1,18 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MaterialChanger : MonoBehaviour
 {
+    public Material testMat;
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   // assigning the Material from the gameobject's Mesh Render Component to the testMat variable
+        testMat = GetComponent<MeshRenderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        testMat.color = Random.ColorHSV();
+
     }
 }
