3ce8dd249d03e9b2b8d0757509956135b5a2cfc1
Rory O Donoghue <irelander99@mail.com>
2024-03-05 19:58:40 +0000

    Commit 23 : Adding additional 2D collider to 'Player' and altering 'Update' Function in script

--- 2024_BSCH_CompSci_R_O_D_3053007/Assets/CharacterControllerScript.cs
+++ 2024_BSCH_CompSci_R_O_D_3053007/Assets/CharacterControllerScript.cs
@@ -9,6 +9,10 @@ public class CharacterControllerScript : MonoBehaviour
     public float acceleration;
 
     public Rigidbody2D myRb;
+
+    public float jumpForce;
+
+    public bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +23,7 @@ public class CharacterControllerScript : MonoBehaviour
     void Update()
     {
         // If the absolute value of the input is greater than 0.1
-        if (Mathf.Abs(Input.GetAxis("Horizontal")) * acceleration > 0.1f){
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) * acceleration > 0.1f && myRb.velocity.magnitude < maxspeed){
             
             //Gets the Input value and multiplies it by acceleration
             myRb.AddForce(new Vector2(Input.GetAxis("Horizontal") * acceleration,0),ForceMode2D.Force);
