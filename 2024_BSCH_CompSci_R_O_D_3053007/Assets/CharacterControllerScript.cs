7e4366e5308f467be3cacc581c4df12fa6a31a46
Rory O Donoghue <irelander99@mail.com>
2024-03-05 14:36:16 +0000

    Commit 21: Adding  code to Script

--- 2024_BSCH_CompSci_R_O_D_3053007/Assets/CharacterControllerScript.cs
+++ 2024_BSCH_CompSci_R_O_D_3053007/Assets/CharacterControllerScript.cs
@@ -7,10 +7,12 @@ public class CharacterControllerScript : MonoBehaviour
     public float maxspeed;
 
     public float acceleration;
+
+    public Rigidbody2D myRb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
