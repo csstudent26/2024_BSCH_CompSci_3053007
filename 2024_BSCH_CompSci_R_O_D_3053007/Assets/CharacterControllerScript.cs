5bfade1c7f3cf175dfc9dae500c89e6d457ae9af
Rory O Donoghue <irelander99@mail.com>
2024-03-12 22:56:18 +0000

    Commit 27 : Adding to CharacterController script

--- 2024_BSCH_CompSci_R_O_D_3053007/Assets/CharacterControllerScript.cs
+++ 2024_BSCH_CompSci_R_O_D_3053007/Assets/CharacterControllerScript.cs
@@ -17,16 +17,19 @@ public class CharacterControllerScript : MonoBehaviour
     public float secondaryJumpForce;
     public float secondaryJumpTime;
     public bool secondaryJump;
+    public Animator anim;
 
     
     void Start()
     {
         myRb = GetComponent<Rigidbody2D>();
+        anim = GetComponentsInChildren<Animator>()
     }
 
     // Update is called once per frame
     void Update()
     {
+        anim.SetFloat("speed", Mathf.Abs(myRb.velocity.x));
         // If the absolute value of the input is greater than 0.1
         if (Mathf.Abs(Input.GetAxis("Horizontal")) * acceleration > 0.1f && myRb.velocity.magnitude < maxspeed){
             
