ca70c2f3934379945a0669f271f67c51cde4f860
Rory O Donoghue <irelander99@mail.com>
2024-03-12 14:23:18 +0000

    Commit 24: Adding 'Jump Force to script 'CharacterController'

--- 2024_BSCH_CompSci_R_O_D_3053007/Assets/CharacterControllerScript.cs
+++ 2024_BSCH_CompSci_R_O_D_3053007/Assets/CharacterControllerScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -28,5 +29,21 @@ public class CharacterControllerScript : MonoBehaviour
             //Gets the Input value and multiplies it by acceleration
             myRb.AddForce(new Vector2(Input.GetAxis("Horizontal") * acceleration,0),ForceMode2D.Force);
         }
+
+        if (isGrounded == true && Input.GetButtonDown("Jump"))
+        {
+            myRb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        isGrounded = true;
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        isGrounded = true;
     }
 }
